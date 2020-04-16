59b01b776409390d417613c4c8e2c8309ddf03ed
sspleo <sspleo@gmail.com>
2020-04-16 10:31:06 +0900

    Initial commit

---
+++ w4_content.py
@@ -0,0 +1,29 @@
+# reserve woords
+True False
+class
+return
+is
+try:
+    pass
+except expression as identifier:
+    pass
+else:
+    pass
+finally:
+    pass
+lambda
+None
+continue
+def
+from
+while
+nonlocal
+global
+with
+yield
+try
+as
+assert
+
+raise
+except
